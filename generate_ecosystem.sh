f3b5f41efff5f6f9464e1d97585c3e91e9fb71e4
Alejandro Fdez Carrera <alej4fc@gmail.com>
2015-09-02 16:46:57 +0200

    add main script

---
+++ generate_ecosystem.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+ROOT=$(pwd)
+
+SDH_PLATFORM="sdh-platform"
+SDH_WEB="sdh-platform-web"
+SDH_ECO="sdh-ecosystem"
+
+Update_Ecosystem() {
+   if [ ! -d $SDH_PLATFORM ]
+   then
+       git clone https://github.com/SmartDeveloperHub/$SDH_PLATFORM $SDH_PLATFORM
+   else
+       cd $SDH_PLATFORM
+       git pull
+       cd $ROOT
+   fi
+   if [ ! -d $SDH_WEB ]
+   then
+       git clone https://github.com/SmartDeveloperHub/$SDH_WEB $SDH_WEB
+   else
+       cd $SDH_WEB
+       git pull
+       cd $ROOT
+   fi
+}
+
+echo " > Updating Repositories ..."
+Update_Ecosystem
+echo "Updated."
+echo " > Creating Ecosystem ..."
+if [ -d $SDH_ECO ]
+then
+   rm -rf $SDH_ECO
+fi
+cp -R $SDH_PLATFORM $SDH_ECO
+cp -R $SDH_WEB/mysql $SDH_ECO      
+cp -R $SDH_WEB/laravel $SDH_ECO        
+cp -R $SDH_WEB/phpmyadmin $SDH_ECO           
+cp docker-compose.yml $SDH_ECO
+rm -rf $SDH_ECO/vagrant
+rm $SDH_ECO/Vagrantfile
+echo "Created."
+cd $SDH_ECO
