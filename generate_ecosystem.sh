67364b7e2cef968e478a2ca0ac5605458d69acd6
fserena <fernando.serena@centeropenmiddleware.com>
2015-10-05 18:48:27 +0200

    Refactor ecosystem generation script

--- generate_ecosystem.sh
+++ generate_ecosystem.sh
@@ -6,42 +6,26 @@ SDH_PLATFORM="sdh-platform"
 SDH_WEB="sdh-platform-web"
 TAG=$1
 
-Update_Ecosystem() {  
-  echo " > Services tag: $TAG"
-  if [ ! -d $SDH_PLATFORM ]
+Update_Part() {
+  echo " > Updating Repository..."
+  if [ ! -d $1 ]
   then
-    git clone https://github.com/SmartDeveloperHub/$SDH_PLATFORM $SDH_PLATFORM
-    cd $SDH_PLATFORM
+    git clone https://github.com/SmartDeveloperHub/$1 $1
+    cd $1
     git checkout $TAG
-    cd ..
   else
-    cd $SDH_PLATFORM
-    git pull
-    cd $ROOT
-  fi
-  if [ ! -d $SDH_WEB ]
-  then
-    git clone https://github.com/SmartDeveloperHub/$SDH_WEB $SDH_WEB
-    cd $SDH_WEB
-    git checkout $TAG
-    cd ..
-  else
-    cd $SDH_WEB
-    git pull
-    cd $ROOT
-  fi
+    cd $1
+    git pull    
+  fi  
+  echo $1 " updated."  
+  echo " > Creating and starting $1..."
+  docker-compose build && docker-compose stop && docker-compose rm -f && docker-compose up -d
+  echo "Done."
+  cd $ROOT
 }
 
-echo " > Updating Repositories..."
-Update_Ecosystem
-echo "Updated."
-echo " > Creating and starting the platform..."
-cd $SDH_PLATFORM
-docker-compose build && docker-compose stop && docker-compose rm -f && docker-compose up -d
-echo "Done."
-echo " > Creating and starting the web environment..."
-cd ../$SDH_WEB
-docker-compose build && docker-compose stop && docker-compose rm -f && docker-compose up -d
-echo "Done."
-echo "Smart Developer Hub should be ready at http://localhost:9003/ in a few minutes."
+echo " > Services tag: $TAG"
+Update_Part $SDH_PLATFORM
+Update_Part $SDH_WEB
 
+echo "Smart Developer Hub should be ready at http://localhost:9003/ in a few minutes."
