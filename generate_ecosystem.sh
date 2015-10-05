2c2a905e04d135bc5580ee66242827b56e2bca96
Fernando Serena <fernando.serena@centeropenmiddleware.com>
2015-10-05 14:17:15 +0200

    Use their own docker-compose files

--- generate_ecosystem.sh
+++ generate_ecosystem.sh
@@ -4,41 +4,44 @@ ROOT=$(pwd)
 
 SDH_PLATFORM="sdh-platform"
 SDH_WEB="sdh-platform-web"
-SDH_ECO="sdh-ecosystem"
+TAG=$1
 
-Update_Ecosystem() {
-   if [ ! -d $SDH_PLATFORM ]
-   then
-       git clone https://github.com/SmartDeveloperHub/$SDH_PLATFORM $SDH_PLATFORM
-   else
-       cd $SDH_PLATFORM
-       git pull
-       cd $ROOT
-   fi
-   if [ ! -d $SDH_WEB ]
-   then
-       git clone https://github.com/SmartDeveloperHub/$SDH_WEB $SDH_WEB
-   else
-       cd $SDH_WEB
-       git pull
-       cd $ROOT
-   fi
+Update_Ecosystem() {  
+  echo " > Services tag: $TAG"
+  if [ ! -d $SDH_PLATFORM ]
+  then
+    git clone https://github.com/SmartDeveloperHub/$SDH_PLATFORM $SDH_PLATFORM
+    cd $SDH_PLATFORM
+    git checkout $TAG
+    cd ..
+  else
+    cd $SDH_PLATFORM
+    git pull
+    cd $ROOT
+  fi
+  if [ ! -d $SDH_WEB ]
+  then
+    git clone https://github.com/SmartDeveloperHub/$SDH_WEB $SDH_WEB
+    cd $SDH_WEB
+    git checkout $TAG
+    cd ..
+  else
+    cd $SDH_WEB
+    git pull
+    cd $ROOT
+  fi
 }
 
-echo " > Updating Repositories ..."
+echo " > Updating Repositories..."
 Update_Ecosystem
 echo "Updated."
-echo " > Creating Ecosystem ..."
-if [ -d $SDH_ECO ]
-then
-   rm -rf $SDH_ECO
-fi
-cp -R $SDH_PLATFORM $SDH_ECO
-cp -R $SDH_WEB/mysql $SDH_ECO      
-cp -R $SDH_WEB/laravel $SDH_ECO        
-cp -R $SDH_WEB/phpmyadmin $SDH_ECO           
-cp docker-compose.yml $SDH_ECO
-rm -rf $SDH_ECO/vagrant
-rm $SDH_ECO/Vagrantfile
-echo "Created."
-cd $SDH_ECO
+echo " > Creating and starting the platform..."
+cd $SDH_PLATFORM
+docker-compose build && docker-compose stop && docker-compose rm -f && docker-compose up -d
+echo "Done."
+echo " > Creating and starting the web environment..."
+cd ../$SDH_WEB
+docker-compose build && docker-compose stop && docker-compose rm -f && docker-compose up -d
+echo "Done."
+echo "Smart Developer Hub should be ready at http://localhost:9003/ in a few minutes."
+
